c3b3d56395b7a2e9952bc34698c24715a297e1e2
Marco Martin <notmart@gmail.com>
2017-12-12 13:33:32 +0100

    first commit

---
+++ usr/share/plasma/look-and-feel/Cupertine/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,82 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [],
+            "config": {
+                "/": {
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/General": {
+                    "ToolBoxButtonState": "bottomright",
+                    "ToolBoxButtonX": "5000",
+                    "ToolBoxButtonY": "5000",
+                    "pressToMove": "false",
+                    "pressToMoveHelp": "false"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "FillMode": "2",
+                    "Image": "MonumentValley",
+                    "height": "1050",
+                    "width": "1680"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "left",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration/General": {
+                            "favoritesPortedToKAstats": "true"
+                        },
+                        "/Shortcuts": {
+                            "global": "Alt+F1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.simplemenu"
+                },
+                {
+                    "plugin": "org.kde.plasma.appmenu"
+                },
+                {
+                    "plugin": "org.kde.plasma.panelspacer"
+                },
+                {
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "plugin": "org.kde.plasma.digitalclock"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "height": 1.5,
+            "hiding": "normal",
+            "location": "top",
+            "maximumLength": 1000,
+            "minimumLength": 1000,
+            "offset": 0
+        }
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
